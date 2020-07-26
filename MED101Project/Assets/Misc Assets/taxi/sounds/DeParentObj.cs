26057336e6c4fa1b1cf10acd6d49948424e094e6
Thomas Pedersen <t_a_p@hotmail.com>
2020-07-26 03:30:52 +0200

    added sounds to taxi and player movement out of taxi

---
+++ MED101Project/Assets/Misc Assets/taxi/sounds/DeParentObj.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeParentObj : MonoBehaviour
+{
+    public GameObject PlayerGO;
+    //private Vector3 OrigPos;
+    public GameObject NewParent;
+    
+    // Start is called before the first frame update
+    public void DeParent()
+    {
+        //OrigPos = PlayerGO.transform.localPosition;
+        PlayerGO.transform.SetParent(NewParent.transform, true);
+        //PlayerGO.transform.localPosition = OrigPos;
+    }
+}
