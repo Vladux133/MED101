476289e39465c4d9cbb231d2ecf65b0d5db319e5
Thomas Pedersen <t_a_p@hotmail.com>
2020-07-31 02:49:42 +0200

    merged taxi with notes (still need one sound to work with taxi)

--- MED101Project/Assets/Misc Assets/taxi/sounds/DeParentObj.cs
+++ MED101Project/Assets/Misc Assets/taxi/sounds/DeParentObj.cs
@@ -11,15 +11,15 @@ public class DeParentObj : MonoBehaviour
     public void DeParent()
     {
         //OrigPos = PlayerGO.transform.localPosition;
-        if (NewParent == null)
-        {
-            PlayerGO.transform.SetParent(null, true);
-            //PlayerGO.transform.position = TaxiObj.transform.position;
-        } else
-        {
-            PlayerGO.transform.SetParent(NewParent.transform, true);
-            //PlayerGO.transform.position = TaxiObj.transform.position;
-        }
+
+        Debug.Log("SETTING TO ZERO");
+        PlayerGO.transform.localPosition = Vector3.zero;
+        Debug.Log(PlayerGO.transform.localPosition);
+        PlayerGO.transform.parent = NewParent.transform;
+
+        //PlayerGO.transform.SetParent(NewParent.transform, true);
+        //PlayerGO.transform.position = TaxiObj.position;
+
         //PlayerGO.transform.localPosition = TaxiObj.transform.localPosition + PlayerGO.transform.localPosition;
         //Debug.Log("TaxiObj Local: " + TaxiObj.transform.localPosition);
         //Debug.Log("PlayerObj Local: " + PlayerGO.transform.localPosition);
